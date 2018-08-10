5061a87c4453759bfa031cb69bc83c01960c47c2
Scott Wales <scott.wales@unimelb.edu.au>
2018-08-10 13:29:39 +1000

    Basic regridding working but untested

---
+++ src/coecms/grid.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# Copyright 2018 ARC Centre of Excellence for Climate Extremes
+# author: Scott Wales <scott.wales@unimelb.edu.au>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import print_function
+from abc import ABC, abstractmethod
+
+import xarray
+import numpy
+
+"""
+Different grid types
+"""
+
+def identify_grid(dataset):
+    """
+    Identify the grid used by a Dataset
+
+    Args:
+        dataset (xarray.Dataset): Input dataset
+
+    Returns:
+        Grid: Grid for that dataset
+    """
+
+    if isinstance(dataset, Grid):
+        return dataset
+    
+    try:
+        if dataset.lon.ndim == 1 and dataset.lat.ndim == 1:
+            return LonLatGrid(lons=dataset.lon, lats=dataset.lat)
+    except AttributeError:
+        pass
+
+    raise NotImplementedError
+
+
+class Grid(ABC):
+
+    @abstractmethod
+    def to_cdo_grid(self, outfile):
+        """
+        Write the grid to a format readable by CDO's regridder (either text or
+        SCRIP format)
+
+        Args:
+            outfile: File-like object to write to
+        """
+        pass
+
+
+    @abstractmethod
+    def to_netcdf(self, outfile):
+        """
+        Create a netCDF file using the grid
+
+        Args:
+            outfile: File-like object to write to
+        """
+
+
+class LonLatGrid(Grid):
+    """
+    A cartesian grid, with lats and lons one dimensional arrays
+    """
+    def __init__(self, lats, lons):
+        """
+        Args:
+            lats (numpy.array): Grid latitudes
+            lons (numpy.array): Grid longitude
+        """
+
+        self.lats = numpy.array(lats)
+        self.lons = numpy.array(lons)
+
+        if self.lats.ndim != 1 or self.lons.ndim != 1:
+            raise Exception("Lons and Lats must be 1D")
+
+    
+    def to_cdo_grid(self, outfile):
+        outfile.write('gridtype = lonlat\n'.encode())
+
+        outfile.write(('xsize = %d\n'%len(self.lons)).encode())
+        outfile.write(('xvals = %s\n'%(','.join(['%f'%x for x in self.lons]))).encode())
+
+        outfile.write(('ysize = %d\n'%len(self.lats)).encode())
+        outfile.write(('yvals = %s\n'%(','.join(['%f'%x for x in self.lats]))).encode())
+
+        outfile.flush()
+
+
+    def to_netcdf(self, outfile):
+        ds = xarray.DataArray(data=numpy.zeros((len(self.lats), len(self.lons))), 
+                coords=[('lat', self.lats), ('lon', self.lons)])
+        ds.to_netcdf(outfile)
+
+
+    def to_scrip(self, outfile):
+        lon = lon % 360
+
+        top = (lat.shift(lat=-1)+lat)/2.0
+        top[-1] = 90
+
+        bot = (lat.shift(lat=1) + lat)/2.0
+        bot[0] = -90
+
+        left  = ((lon - (lon - lon.roll(lon=1).values)%360)/2.0) % 360
+        right = (lon + ((lon.roll(lon=-1).values-lon)%360)/2.0) % 360
+
+        center_lon, center_lat = numpy.meshgrid(lon, lat)
+
+        corner_lon0, corner_lat0 = numpy.meshgrid(left, top)
+        corner_lon1, corner_lat1 = numpy.meshgrid(left, bot)
+        corner_lon2, corner_lat2 = numpy.meshgrid(right, bot)
+        corner_lon3, corner_lat3 = numpy.meshgrid(right, top)
+
+        corner_lat = numpy.array([x.reshape(-1) for x in [corner_lat0, corner_lat1, corner_lat2, corner_lat3]])
+        corner_lon = numpy.array([x.reshape(-1) for x in [corner_lon0, corner_lon1, corner_lon2, corner_lon3]])
+
+        scrip = xarray.Dataset(
+                coords = {
+                    'grid_dims': (['grid_rank'], [lon.size,lat.size]),
+                    'grid_center_lat': (['grid_size'], center_lat.reshape(-1)),
+                    'grid_center_lon': (['grid_size'], center_lon.reshape(-1)),
+                    'grid_imask': (['grid_size'], numpy.ones(center_lat.size)),
+                    'grid_corner_lat': (['grid_size', 'grid_corners'], corner_lat.T),
+                    'grid_corner_lon': (['grid_size', 'grid_corners'], corner_lon.T),
+                    })
+
+        scrip.grid_center_lat.attrs['units'] = 'degrees_north'
+        scrip.grid_center_lon.attrs['units'] = 'degrees_east'
+        scrip.grid_corner_lat.attrs['units'] = 'degrees_north'
+        scrip.grid_corner_lon.attrs['units'] = 'degrees_east'
+
+        scrip.to_netcdf(outfile)
+
+
---
+++ src/coecms/regrid.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# Copyright 2018 ARC Centre of Excellence for Climate Extremes
+# author: Scott Wales <scott.wales@unimelb.edu.au>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import print_function
+
+from coecms.grid import identify_grid
+
+import subprocess
+import xarray
+import numpy
+import tempfile
+import scipy.sparse
+
+
+def cdo_generate_weights(source_data, target_grid, method):
+    """
+    Generate weights for regridding using CDO
+
+    Args:
+        source_data (xarray.Dataset): Source dataset
+        target_grid (string): Path to file containing the target grid
+            description
+        method: Regridding method
+
+    Returns:
+        xarray.Dataset: Regridding weights
+    """
+
+    source_data_file = tempfile.NamedTemporaryFile()
+    target_grid_file = tempfile.NamedTemporaryFile()
+    weight_file = tempfile.NamedTemporaryFile()
+
+    try:
+        identify_grid(source_data).to_netcdf(source_data_file.name)
+        identify_grid(target_grid).to_cdo_grid(target_grid_file)
+
+        subprocess.check_output([
+            "cdo",
+            "genbil,%s"%target_grid_file.name,
+            source_data_file.name,
+            weight_file.name],
+            stderr=subprocess.PIPE)
+
+        weights = xarray.open_dataset(weight_file.name)
+        return weights
+
+    finally:
+        source_data_file.close()
+        target_grid_file.close()
+        weight_file.close()
+
+
+
+def apply_weights(source_data, weights):
+    """
+    Apply the CDO weights ``weights`` to ``source_data``, performing a regridding operation
+
+    Args:
+        source_data (xarray.Dataset): Source dataset
+        weights (xarray.Dataset): CDO weights information
+
+    Returns:
+        xarray.Dataset: Regridded version of the source dataset
+    """
+    w = weights
+
+    weight_matrix = scipy.sparse.coo_matrix((w.remap_matrix[:,0],
+        (w.src_address.data - 1, w.dst_address.data -1)))
+
+    lat = xarray.DataArray(w.dst_grid_center_lat.data.reshape(w.dst_grid_dims.data),
+            name='lat', attrs = w.dst_grid_center_lat.attrs, dims=['i','j'])
+
+    lon = xarray.DataArray(w.dst_grid_center_lon.data.reshape(w.dst_grid_dims.data),
+            name='lon', attrs = w.dst_grid_center_lon.attrs, dims=['i','j'])
+
+    data = (source_data.data.reshape(-1) * weight_matrix).reshape(w.dst_grid_dims.data)
+    return xarray.DataArray(data, dims=['i', 'j'], coords={'lat': lat, 'lon': lon},
+            name=source_data.name, attrs=source_data.attrs)
+
+
+class Regridder(object):
+    def __init__(self, source_grid, target_grid, method):
+        """
+        Set up the regridding operation
+
+        Args:
+            source_grid (coecms.regrid.Grid or xarray.Dataset): Source grid / sample dataset
+            target_grid (coecms.regrid.Grid or xarray.Dataset): Target grid / sample dataset
+            method: Regridding method
+        """
+        self._weights = cdo_generate_weights(source_grid, target_grid, method)
+
+
+    def regrid(self, source_data):
+        """
+        Regrid the xarray.Dataset ``source_data`` to match the target grid
+
+        Args:
+            source_data (xarray.Dataset): Source dataset
+
+        Returns:
+            xarray.Datset: Regridded version of the source dataset
+        """
+
+        return apply_weights(source_data, self._weights)
+
+
+def regrid(source_data, target_grid, method):
+    """
+    A simple regrid. Inefficient if you are regridding more than one dataset
+    to the target grid because it re-generates the weights each time
+
+    Args:
+        source_data (xarray.Dataset): Source dataset
+        target_grid (coecms.regrid.Grid or xarray.Dataset): Target grid / sample dataset
+        method: Regridding method
+
+    Returns:
+        xarray.Datset: Regridded version of the source dataset
+    """
+
+    regridder = Regridder(source_data, target_grid, method)
+
+    return regridder.regrid(source_data)
