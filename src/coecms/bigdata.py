3dc2aaffd6d388210ae519bae98d5be0ab56a5b7
Scott Wales <scott.wales@unimelb.edu.au>
2018-10-25 13:54:37 +1100

    New approach to avoid threading errors

--- src/coecms/bigdata.py
+++ src/coecms/bigdata.py
@@ -18,12 +18,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import dask
-import netCDF4
 import xarray
+import dask
 
 
-def unsafe_mfdataset(path_series, dim='time'):
+def unsafe_mfdataset(paths, concat_dim='time', chunks={}, decode_cf=True,
+        decode_times=True, **kwargs):
     """Virtually concatenate files, without checking dimensions
 
     xarray.open_mfdataset will check each file's dimensions to make sure they
@@ -32,121 +32,42 @@ def unsafe_mfdataset(path_series, dim='time'):
     a published dataset.
 
     Args:
-        path_series (pandas.Series): A Pandas series indexed by the whole
-            dataset's `dim` axis, with values of the NetCDF4-compatible path
-            (file path or OpenDAP URL) containing that value of `dim`.
+        paths (iterable of str): An iterable with the path names to open. Files
+            will be concatenated in this order
 
-            It is assumed that all values of the file's `dim` axis are present
-            in the index, the values of `dim` in the file are in the same
-            order as the index, and that the index values for each individual
-            file are contiguous.
+        concat_dim (str): Dimension name to concatenate along
 
-        dim (str): Dimension name to concatenate along
+        chunks (dict): Dask chunk size
+
+        decode_cf (bool): Decode CF metadata
+
+        decode_times (bool): Decode CF time axis to Python datetimes
+
+        **kwargs: Passed to xarray.open_dataset
 
     Returns: xarray.Dataset containing a virtual aggregate of the files in
-        ``path_series``. The returned dataset's ``dim`` coordinate will be the
-        index of ``path_series``
+        ``paths``.
     """
-    # Group the series by the path value, so we have groups of (path, [time_axis])
-    groups = path_series.groupby(path_series).groups
 
-    paths = list(groups.keys())
-    path_dates = list(groups.values())
+    fs = []
+    for p in paths:
+        fs.append(xarray.open_dataset(p, chunks=chunks, decode_cf=False, **kwargs))
 
-    # Open just the first path, to get the metadata
-    mold_ds = xarray.open_dataset(paths[0])
+    ds = xarray.Dataset(attrs=fs[0].attrs)
 
-    # Work out which variables contain ``dim`` and which do not
-    aggregate_vars = []
-    static_vars = []
-    for k, v in mold_ds.variables.items():
-        if dim in v.dims:
-            aggregate_vars.append(k)
+    for k,v in fs[0].variables.items():
+        if concat_dim in v.dims:
+            concat_axis = v.dims.index(concat_dim)
+            ds[k] = xarray.DataArray(
+                    dask.array.concatenate([f[k].data for f in fs], axis=concat_axis),
+                    attrs=v.attrs,
+                    dims=v.dims,
+                    )
         else:
-            static_vars.append(k)
+            ds[k] = v
 
-    # Get the dask arrays for each file
-    # delayed_data is a list of dicts, each dict having the variables of a single file
-    delayed_data = []
-    for path, dates in groups.items():
-        delayed_data.append(virtual_dataset(
-            path, aggregate_vars, mold_ds, dim, dates))
-
-    # Construct the new dataset
-    ds = xarray.Dataset()
-    ds.attrs = mold_ds.attrs
-
-    # Construct DataArrays for each variable
-    for v in aggregate_vars:
-        # Aggregate Dask arrays
-        concat_axis = mold_ds[v].dims.index(dim)
-        data = dask.array.concatenate(
-            [d[v] for d in delayed_data], axis=concat_axis)
-
-        # Copy metadata from the mold dataset
-        da = xarray.DataArray(data,
-                              dims=mold_ds[v].dims,
-                              attrs=mold_ds[v].attrs,
-                              name=mold_ds[v].name)
-
-        # Add the now concatenated DataArray to the output dataset
-        ds[v] = da
-
-    # Copy the non-collated variables from the first file
-    for v in static_vars:
-        ds[v] = mold_ds[v]
+    if decode_cf:
+        ds = xarray.decode_cf(ds, decode_times=decode_times)
 
     return ds
 
-
-@dask.delayed
-def delayed_open(path):
-    """Delayed open of a single file
-
-    Use this with dask.array.from_delayed to create a lazily loaded array
-    without actually opening a netCDF file
-    """
-    return netCDF4.Dataset(path)
-
-
-def virtual_dataset(path, aggregate_vars, mold_ds, dim, dim_values):
-    """Prepare Dask arrays for each variables in the file, without opening it
-
-    Only opens the file when the variables needs to be read
-
-    Args:
-        path (str): netCDF4-compatible path to open
-        aggregate_vars (list of str): Variable names to return from the file
-        mold_ds (netCDF4.Dataset): Sample file showing the variable types and sizes
-        dim (str): Dimension name that changes between paths
-        dim_values (numpy.array): Values of ``dim`` for this path
-
-    Returns: dict mapping each variable name in ``aggregate_vars`` to a
-        dask.array containing the values of that variable in ``path``
-    """
-    # Delayed opening of the file
-    ds = delayed_open(path)
-
-    data = {}
-
-    for v in aggregate_vars:
-        if v == dim:  # No need for a Dask array
-            data[v] = dim_values
-            continue
-
-        # Get the variable attributes from the mold Dataset
-        dtype = mold_ds[v].dtype
-        shape = list(mold_ds[v].shape)
-
-        # Overwrite the size on the concat dimension
-        try:
-            concat_axis = mold_ds[v].dims.index(dim)
-        except AttributeError:
-            # NetCDF4 instead of xarray
-            concat_axis = mold_ds[v].dimensions.index(dim)
-        shape[concat_axis] = dim_values.size
-
-        # Construct a Dask array for the variable
-        data[v] = dask.array.from_delayed(ds[v], shape=shape, dtype=dtype)
-
-    return data
--- test/test_bigdata.py
+++ test/test_bigdata.py
@@ -27,61 +27,6 @@ import pandas
 import xarray
 
 
-def test_virtual_dataset(tmpdir):
-    # I can create an empty dataset
-    mold_ds = xarray.Dataset()
-    ds = virtual_dataset('/dev/null', [], mold_ds, 'time', [])
-    assert len(ds) == 0
-
-    # Only the variables in ``aggregate_vars`` are created
-    mold_ds = xarray.Dataset()
-    mold_ds['a'] = xarray.DataArray(numpy.zeros((2,)), dims=['time'])
-    ds = virtual_dataset('/dev/null', [], mold_ds, 'time', numpy.array([1, 2]))
-    assert len(ds) == 0
-    ds = virtual_dataset('/dev/null', ['a'],
-                         mold_ds, 'time', numpy.array([1, 2]))
-    assert len(ds) == 1
-    assert ds['a'].shape == (2,)
-
-    # The ``dim`` axis comes from ``dim_values``, not mold
-    mold_ds = xarray.Dataset()
-    mold_ds['a'] = xarray.DataArray(numpy.zeros((6, 3)), dims=['time', 'x'])
-    ds = virtual_dataset('/dev/null', ['a'],
-                         mold_ds, 'time', numpy.array([1, 2]))
-    assert ds['a'].shape == (2, 3)
-
-    # Dimension ordering doesn't matter
-    mold_ds = xarray.Dataset()
-    mold_ds['a'] = xarray.DataArray(numpy.zeros((3, 6)), dims=['x', 'time'])
-    ds = virtual_dataset('/dev/null', ['a'],
-                         mold_ds, 'time', numpy.array([1, 2]))
-    assert ds['a'].shape == (3, 2)
-
-    # Same behaviour when mold_ds is a netCDF4 dataset
-    mold_ds = xarray.Dataset()
-    mold_ds['a'] = xarray.DataArray(
-        numpy.random.rand(3, 6), dims=['x', 'time'])
-    a_path = tmpdir.join('a.nc')
-    mold_ds.to_netcdf(a_path)
-    mold_ds = netCDF4.Dataset(a_path)
-    ds = virtual_dataset(a_path, ['a'], mold_ds,
-                         'time', numpy.array([1, 2, 3, 4, 5, 6]))
-    assert ds['a'].shape == (3, 6)
-
-    # Test using separate mold and source datasets
-    # Data should come from the source file
-    target_ds = xarray.Dataset()
-    target_ds['a'] = xarray.DataArray(
-        numpy.random.rand(3, 5), dims=['x', 'time'])
-    b_path = tmpdir.join('b.nc')
-    target_ds.to_netcdf(b_path)
-    ds = virtual_dataset(b_path, ['a'], mold_ds,
-                         'time', numpy.array([7, 8, 9, 10, 11]))
-    assert ds['a'].shape == (3, 5)
-    assert type(ds['a']) == dask.array.Array
-    numpy.testing.assert_array_equal(ds['a'], target_ds['a'])
-
-
 def test_unsafe_mfdataset(tmpdir):
     path0 = tmpdir.join('0.nc')
     ds0 = xarray.Dataset()
@@ -93,11 +38,7 @@ def test_unsafe_mfdataset(tmpdir):
     ds1['a'] = xarray.DataArray(numpy.random.rand(3, 6), dims=['x', 'time'])
     ds1.to_netcdf(path1)
 
-    s0 = pandas.Series(path0, index=range(6))
-    s1 = pandas.Series(path1, index=range(6, 12))
-    s = pandas.concat([s0, s1])
-
-    ds = unsafe_mfdataset(s)
+    ds = unsafe_mfdataset([path0, path1])
     assert ds['a'].shape == (3, 12)
     assert ds['time'].shape == (12,)
     numpy.testing.assert_array_equal(ds['time'], range(12))
