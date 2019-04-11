14008e909dd8673efc5c9f1616098230550720f1
Scott Wales <scott.wales@unimelb.edu.au>
2019-04-11 16:57:33 +1000

    um2oasis help strings

--- scripts/um/um2oasis.py
+++ scripts/um/um2oasis.py
@@ -16,35 +16,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Sets up Oasis regridding weights for an ACCESS coupled run
+
+Creates corrected UM mask and land fraction (from interpolating the MOM mask),
+Oasis masks.nc, grids.nc and areas.nc input files and conservative and patch
+regridding weight files
+"""
 
 from coecms.um.um2oasis import *
+import argparse
+import os
 
 
 def main():
-    maskfile = '/g/data1a/access/TIDS/UM/ancil/atmos/n48e/orca1/land_sea_mask/etop01/v1/qrparm.landfrac'
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--lfrac', required=True, help='UM Land Fraction file')
+    parser.add_argument('--lmask', required=True, help='UM Land Mask file')
+    parser.add_argument('--mom', required=True, help='MOM grid_spec.nc file')
+    parser.add_argument('--output','-o', required=True, help='Output directory')
+    args = parser.parse_args()
+
+
+    maskfile = args.lfrac
     frac_iris = iris.load_cube(maskfile, iris.AttributeConstraint(STASH='m01s00i505'))
     frac_iris.coord('latitude').var_name = 'lat'
     frac_iris.coord('longitude').var_name = 'lon'
     um_grid = xarray.DataArray.from_iris(frac_iris)
 
-    mom_gridspec = xarray.open_dataset('/projects/access/access-cm2/input_b/mom4/grid_spec.auscom.20110618.nc')
+    mom_gridspec = xarray.open_dataset(args.mom)
 
     lfrac = create_um_lfrac_from_mom(mom_gridspec, um_grid)
-    correct_ancils(lfrac)
-
-    print(lfrac)
-    lfrac.plot()
-    plt.show()
-
+    correct_ancils(lfrac, mask_ancil=args.lmask, frac_ancil=args.lfrac, outdir=args.output)
     
     scrip_grids = um_endgame_scrip_grids(lfrac)
     scrip_grids['momt'] = mom_t_scrip_grid(mom_gridspec)
 
     oasis_grids = merge_scrip_for_oasis(scrip_grids)
 
-    oasis_grids['masks'].to_netcdf('masks.nc')
-    oasis_grids['grids'].to_netcdf('grids.nc')
-    oasis_grids['areas'].to_netcdf('areas.nc')
+    oasis_grids['masks'].to_netcdf(os.path.join(args.output, 'masks.nc'))
+    oasis_grids['grids'].to_netcdf(os.path.join(args.output, 'grids.nc'))
+    oasis_grids['areas'].to_netcdf(os.path.join(args.output, 'areas.nc'))
 
     for grid in ['um_t']:
         weights = esmf_generate_weights(
@@ -55,7 +67,7 @@ def main():
                 extrap_method='none',
                 ignore_unmapped=False,
                 )
-        rename_weights_esmf_to_scrip(weights).to_netcdf(f'rmp_momt_to_{grid}_CONSERV_FRACAREA.nc')
+        rename_weights_esmf_to_scrip(weights).to_netcdf(os.path.join(args.output,f'rmp_momt_to_{grid}_CONSERV_FRACAREA.nc'))
         weights = esmf_generate_weights(
                 scrip_grids[grid].reset_index('grid_size'),
                 scrip_grids['momt'].reset_index('grid_size'),
@@ -64,7 +76,7 @@ def main():
                 extrap_method='none',
                 ignore_unmapped=False,
                 )
-        rename_weights_esmf_to_scrip(weights).to_netcdf(f'rmp_{grid}_to_momt_CONSERV_FRACAREA.nc')
+        rename_weights_esmf_to_scrip(weights).to_netcdf(os.path.join(args.output,f'rmp_{grid}_to_momt_CONSERV_FRACAREA.nc'))
 
     for grid in ['um_t', 'um_u', 'um_v']:
         weights = esmf_generate_weights(
@@ -76,7 +88,7 @@ def main():
                 pole='none',
                 ignore_unmapped=True,
                 )
-        rename_weights_esmf_to_scrip(weights).to_netcdf(f'rmp_momt_to_{grid}_PATCH.nc')
+        rename_weights_esmf_to_scrip(weights).to_netcdf(os.path.join(args.output,f'rmp_momt_to_{grid}_PATCH.nc'))
         weights = esmf_generate_weights(
                 scrip_grids[grid].reset_index('grid_size'),
                 scrip_grids['momt'].reset_index('grid_size'),
@@ -86,7 +98,7 @@ def main():
                 pole='none',
                 ignore_unmapped=True,
                 )
-        rename_weights_esmf_to_scrip(weights).to_netcdf(f'rmp_{grid}_to_momt_PATCH.nc')
+        rename_weights_esmf_to_scrip(weights).to_netcdf(os.path.join(args.output,f'rmp_{grid}_to_momt_PATCH.nc'))
 
 if __name__ == '__main__':
     main()
